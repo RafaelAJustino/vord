1268e80e08732852bd7ae99f7bd6e4f6241f97ca
Rafael Justino <rafael.angelo.justino@gmail.com>
2024-10-21 14:45:44 -0300

    update

--- script.js
+++ script.js
@@ -42,57 +42,57 @@ const desafios = [
     'escolha uma pessoa, ela somente pode escolher desafio',
     'você somente pode escolher desafio',
     'passe a língua suavemente no pescoço de alguém à sua escolha (10 segundos)',
-    // Desafios novos
-    'faça um strip-tease para o grupo até ficar de roupa íntima',
-    'escolha alguém para dar um beijo longo na boca (15 segundos)',
-    'dê um beijo molhado no peito ou seio de alguém à sua escolha (10 segundos)',
-    'venda seus olhos e deixe alguém te beijar em qualquer parte do corpo (20 segundos)',
-    'faça uma massagem sensual na pessoa à sua frente (1 minuto)',
-    'dê uma mordida suave no lábio de alguém à sua escolha (10 segundos)',
-    'simule uma cena de sexo com alguém à sua esquerda (30 segundos)',
-    'escolha duas pessoas para simular uma cena íntima com você (30 segundos)',
-    'troque uma peça de roupa íntima com alguém do grupo (se possível)',
-    'dê um tapa erótico na bunda da pessoa à sua frente',
-    'deixe a pessoa à sua esquerda acariciar suas costas de maneira sensual (30 segundos)',
-    'escolha alguém para te beijar por cima da roupa em uma área íntima (10 segundos)',
-    'faça uma pose erótica com alguém à sua escolha e segure por 30 segundos',
-    'faça uma dança sensual no colo da pessoa à sua direita (1 minuto)',
-    'dê uma lambida no abdômen de alguém à sua escolha (10 segundos)',
-    'se você for mulher, deixe um homem tirar sua calcinha com os dentes (se possível)',
-    'faça uma dança provocativa para o grupo (1 minuto)',
-    'deixe alguém te beijar da cabeça aos pés (exceto partes íntimas)',
-    'simule uma posição sexual com uma pessoa à sua escolha (30 segundos)',
-    'coloque uma venda e deixe o grupo te tocar em três partes diferentes',
+    // Desafios novos
+    'faça um strip-tease para o grupo até ficar de roupa íntima',
+    'escolha alguém para dar um beijo longo na boca (15 segundos)',
+    'dê um beijo molhado no peito ou seio de alguém à sua escolha (10 segundos)',
+    'venda seus olhos e deixe alguém te beijar em qualquer parte do corpo (20 segundos)',
+    'faça uma massagem sensual na pessoa à sua frente (1 minuto)',
+    'dê uma mordida suave no lábio de alguém à sua escolha (10 segundos)',
+    'simule uma cena de sexo com alguém à sua esquerda (30 segundos)',
+    'escolha duas pessoas para simular uma cena íntima com você (30 segundos)',
+    'troque uma peça de roupa íntima com alguém do grupo (se possível)',
+    'dê um tapa erótico na bunda da pessoa à sua frente',
+    'deixe a pessoa à sua esquerda acariciar suas costas de maneira sensual (30 segundos)',
+    'escolha alguém para te beijar por cima da roupa em uma área íntima (10 segundos)',
+    'faça uma pose erótica com alguém à sua escolha e segure por 30 segundos',
+    'faça uma dança sensual no colo da pessoa à sua direita (1 minuto)',
+    'dê uma lambida no abdômen de alguém à sua escolha (10 segundos)',
+    'se você for mulher, deixe um homem tirar sua calcinha com os dentes (se possível)',
+    'faça uma dança provocativa para o grupo (1 minuto)',
+    'deixe alguém te beijar da cabeça aos pés (exceto partes íntimas)',
+    'simule uma posição sexual com uma pessoa à sua escolha (30 segundos)',
+    'coloque uma venda e deixe o grupo te tocar em três partes diferentes',
     'faça um gemido sensual no ouvido da pessoa à sua esquerda',
-    'deixe alguém te tocar em uma área íntima ( ela escolhe se é por cima da roupa ou não) (10 segundos)',
-    'todos ficam sem roupa e tiram uma foto em grupo para guardar o momento',
-    'faça um strip-tease para alguém específico (até ficar sem roupa)',
-    'escolha alguém para dar um beijo no seu pescoço e descer até a sua barriga (20 segundos)',
-    'simule um beijo apaixonado no pescoço de alguém por trás (15 segundos)',
-    'faça uma dança sensual para o grupo enquanto tira uma peça de roupa',
-    'deixe a pessoa à sua frente passar a mão por baixo de sua roupa e te tocar onde desejar (30 segundos)',
-    'sussurre uma fantasia erótica no ouvido da pessoa à sua direita',
-    'deixe uma pessoa à sua escolha te beijar onde ela quiser (20 segundos)',
-    'beije ou lamba os dedos da pessoa à sua frente (15 segundos)',
-    'deixe a pessoa à sua esquerda te despir até a roupa íntima (se ainda estiver vestindo)',
-    'dê uma mordidinha suave no bumbum da pessoa à sua escolha',
-    'venda seus olhos e deixe alguém tocar seus lábios com uma parte do corpo; você tem que adivinhar qual parte (10 segundos)',
-    'deixe alguém tirar sua roupa íntima e troque de lugar com essa pessoa',
-    'escolha alguém para massagear suas partes íntimas por cima da roupa (15 segundos)',
-    'faça uma pose sexual em dupla e deixe alguém do grupo fotografar (utilizando as roupas que estão vestidos)',
-    'beije os pés de alguém (10 segundos)',
-    'imite sua posição sexual favorita com alguém do grupo (30 segundos)',
-    'deixe a pessoa à sua direita escolher uma parte do seu corpo para você expor (10 segundos)',
-    'sente no colo de alguém e provoque (1 minuto)',
-    'deixe alguém te beijar por cima da roupa nas partes íntimas (15 segundos)',
-    'escolha alguém para receber um "beijo erótico" que percorre do pescoço até o umbigo',
-    'deixe duas pessoas do grupo tirarem sua roupa até ficar de roupa íntima',
-    'faça uma dança provocativa em frente a alguém e deixe-o te tocar levemente (30 segundos)',
-    'seja amarrado(a) (de leve) e deixe uma pessoa te tocar levemente (30 segundos)',
-    'mande um nude explícito para todos da roda',
-    'todos da roda te mandam um nude explícito',
-    'escolha alguém, vocês devem se juntar e mandar um nude explícito para todos da roda',
-    'beije a pessoa à sua direita em um lugar que você nunca beijou antes (10 segundos)',
+    'deixe alguém te tocar em uma área íntima ( ela escolhe se é por cima da roupa ou não) (10 segundos)',
+    'todos ficam sem roupa e tiram uma foto em grupo para guardar o momento',
+    'faça um strip-tease para alguém específico (até ficar sem roupa)',
+    'escolha alguém para dar um beijo no seu pescoço e descer até a sua barriga (20 segundos)',
+    'simule um beijo apaixonado no pescoço de alguém por trás (15 segundos)',
+    'faça uma dança sensual para o grupo enquanto tira uma peça de roupa',
+    'deixe a pessoa à sua frente passar a mão por baixo de sua roupa e te tocar onde desejar (30 segundos)',
+    'sussurre uma fantasia erótica no ouvido da pessoa à sua direita',
+    'deixe uma pessoa à sua escolha te beijar onde ela quiser (20 segundos)',
+    'beije ou lamba os dedos da pessoa à sua frente (15 segundos)',
+    'deixe a pessoa à sua esquerda te despir até a roupa íntima (se ainda estiver vestindo)',
+    'dê uma mordidinha suave no bumbum da pessoa à sua escolha',
+    'venda seus olhos e deixe alguém tocar seus lábios com uma parte do corpo; você tem que adivinhar qual parte (10 segundos)',
+    'deixe alguém tirar sua roupa íntima e troque de lugar com essa pessoa',
+    'escolha alguém para massagear suas partes íntimas por cima da roupa (15 segundos)',
+    'faça uma pose sexual em dupla e deixe alguém do grupo fotografar (utilizando as roupas que estão vestidos)',
+    'beije os pés de alguém (10 segundos)',
+    'imite sua posição sexual favorita com alguém do grupo (30 segundos)',
+    'deixe a pessoa à sua direita escolher uma parte do seu corpo para você expor (10 segundos)',
+    'sente no colo de alguém e provoque (1 minuto)',
+    'deixe alguém te beijar por cima da roupa nas partes íntimas (15 segundos)',
+    'escolha alguém para receber um "beijo erótico" que percorre do pescoço até o umbigo',
+    'deixe duas pessoas do grupo tirarem sua roupa até ficar de roupa íntima',
+    'faça uma dança provocativa em frente a alguém e deixe-o te tocar levemente (30 segundos)',
+    'seja amarrado(a) (de leve) e deixe uma pessoa te tocar levemente (30 segundos)',
+    'mande um nudes explícito para todos da roda',
+    'todos da roda te mandam um nudes explícito',
+    'escolha alguém, vocês devem se juntar e mandar um nudes explícito para todos da roda',
+    'beije a pessoa à sua direita em um lugar que você nunca beijou antes (10 segundos)',
     'tire uma peça de roupa e faça uma pose sexy para o grupo (não pode ser calçados, meias ou acessórios)',
 ];
 
@@ -166,22 +166,148 @@ const verdades = [
     'qual foi a situação mais embaraçosa que já passou enquanto fazia sexo?',
 ];
 
+const euNunca = [
+    "enviei uma foto íntima.",
+    "usei brinquedos durante o sexo.",
+    "fiz sexo em um lugar público.",
+    "fui pego(a) no flagra enquanto fazia sexo.",
+    "tive um sonho erótico com alguém do grupo.",
+    "beijei mais de uma pessoa na mesma noite.",
+    "pratiquei sexting.",
+    "transei em uma festa.",
+    "transei no carro.",
+    "tive uma noite de sexo casual.",
+    "fiz sexo sem estar sóbrio(a).",
+    "participei de um ménage à trois.",
+    "transei em uma piscina ou no mar.",
+    "fiz sexo oral em alguém.",
+    "dormi com alguém e me arrependi no dia seguinte.",
+    "traí em um relacionamento.",
+    "assisti a vídeos pornográficos.",
+    "usei algemas ou vendas durante o sexo.",
+    "tive um crush em alguém comprometido.",
+    "transei com alguém que conheci online.",
+    "menti sobre o número de parceiros sexuais que tive.",
+    "fui a um clube de striptease.",
+    "fiz sexo em um local de trabalho.",
+    "enviei nudes para a pessoa errada.",
+    "tive fantasias com alguém do mesmo sexo.",
+    "fiquei com mais de uma pessoa no mesmo dia.",
+    "dei ou recebi um tapa durante o sexo.",
+    "fiz uma massagem sensual em alguém.",
+    "recebi ou fiz um strip-tease.",
+    "transei em uma varanda ou terraço.",
+    "fiz sexo com alguém que acabei de conhecer.",
+    "tive um sonho erótico com meu chefe ou colega de trabalho.",
+    "transei em um banheiro público.",
+    "tive uma experiência sexual em um avião.",
+    "disse 'eu te amo' para conseguir sexo.",
+    "assisti a um filme adulto com alguém.",
+    "fiz sexo na casa de outra pessoa.",
+    "experimentei uma posição sexual incomum.",
+    "tive um encontro sexual com alguém mais velho.",
+    "tive um encontro sexual com alguém mais jovem.",
+    "usei uma fantasia erótica.",
+    "tive uma relação de uma noite.",
+    "enviei mensagens de teor sexual para a pessoa errada.",
+    "fiz sexo no chuveiro.",
+    "fiz sexo com alguém apenas pela aparência.",
+    "fui para a cama com alguém só por causa do álcool.",
+    "usei a desculpa de estar doente para não fazer sexo.",
+    "pesquisei sobre fetiches na internet.",
+    "assisti pornografia com meu parceiro(a).",
+    "pensei em outra pessoa enquanto fazia sexo.",
+    "usei óleos ou cremes durante o sexo.",
+    "fiz sexo em frente a um espelho.",
+    "fui a um clube de swing.",
+    "fiz sexo sem preservativo com alguém que não conhecia bem.",
+    "tirei fotos íntimas de outra pessoa.",
+    "fiquei excitado(a) com uma cena de filme.",
+    "beijei alguém do mesmo sexo.",
+    "tive uma experiência de voyeurismo.",
+    "transei em um hotel.",
+    "usei comida durante o sexo.",
+    "gritei o nome errado durante o sexo.",
+    "tive um orgasmo fingido.",
+    "fiz sexo com alguém comprometido.",
+    "me apaixonei por alguém só por causa do sexo.",
+    "tentei recriar uma cena de filme pornô.",
+    "fiz sexo com luzes acesas.",
+    "transei em um camping ou acampamento.",
+    "tive uma conversa explícita no telefone.",
+    "pesquisei novas posições sexuais para tentar.",
+    "tive um parceiro(a) que usava lingerie especial.",
+    "vi alguém nu(a) por acidente.",
+    "tive uma fantasia com um(a) amigo(a) próximo(a).",
+    "tive um desejo por alguém do grupo.",
+    "fui convidado(a) para um ménage à trois.",
+    "flertei com alguém comprometido.",
+    "fiquei excitado(a) em um local inadequado.",
+    "fiz sexo com um(a) ex.",
+    "transei em uma festa de casamento.",
+    "brinquei de verdade ou consequência com perguntas eróticas.",
+    "enviei uma mensagem de teor sexual para um(a) ex.",
+    "enviei flores ou presentes para alguém após uma noite juntos.",
+    "fiz sexo sem estar completamente interessado(a).",
+    "fiz sexo com mais de uma pessoa em uma semana.",
+    "fiquei com alguém apenas por status.",
+    "fui convidado(a) para um encontro através de aplicativos.",
+    "enviei uma mensagem de teor sexual logo depois de conhecer alguém.",
+    "fiquei com alguém que era casado(a).",
+    "tive uma experiência sexual com alguém famoso(a).",
+    "fui a uma festa só para tentar sexo com alguém."
+];
+
+
+function selectGame(button) {
+    const games = document.getElementById('selectGame');
+    button.classList.add('active');
+
+    setTimeout(() => {
+        button.classList.remove('active');
+    }, 200);
+
+    games.style.display = 'none';
+    games.classList.remove('active')
+
+    if (button.value == 'VORD') {
+        document.getElementById('vord').classList.add('active');
+        document.title = 'FUN GAMES - V || D +18'
+    } else if (button.value == 'NEVER') {
+        document.getElementById('never').classList.add('active');
+        document.title = 'FUN GAMES - EU NUNCA'
+    }
+}
+
+function goInit() {
+    const games = document.getElementById('selectGame');
+
+    games.style.display = 'flex';
+    games.classList.add('active')
+
+    document.getElementById('vord').classList.remove('active');
+    document.getElementById('never').classList.remove('active');
+    document.title = 'FUN GAMES'
+}
+
+//------------------------------ VERDADE OU DESAFIO ------------------------------
+
 let verdadeAtivo = '';
 
-const getText = () => {
-    return document.getElementById('text')
+const getText = (val) => {
+    return document.querySelector(val);
 }
 
-const getDivBtns = () => {
-    return document.getElementById('divBtns')
+const getDivBtnsVord = () => {
+    return document.getElementById('divBtnsVord')
 }
 
-const getBtnD = () => {
-    return document.getElementById('desafio')
+const getBtnDVord = () => {
+    return document.getElementById('desafioVord')
 }
 
-const getBtnV = () => {
-    return document.getElementById('verdade')
+const getBtnVVord = () => {
+    return document.getElementById('verdadeVord')
 }
 
 function getRandomElement(array) {
@@ -197,38 +323,53 @@ const onClickDesafio = (button) => {
 
     if (tempText == desafioAtivo) {
         desafioAtivo = tempText;
-        getText().innerHTML = desafioAtivo;
+        getText('#vord .text').innerHTML = desafioAtivo;
     } else {
         desafioAtivo = getRandomElement(desafios);
-        getText().innerHTML = desafioAtivo;
+        getText('#vord .text').innerHTML = desafioAtivo;
     }
 
     if (desafioAtivo == 'verdade agora não é uma opção') {
-        getBtnV().setAttribute("disabled", "");
+        getBtnVVord().setAttribute("disabled", "");
     }
 
-    getText().classList.add('active');
+    getText('#vord .text').classList.add('active');
     button.classList.add('active');
 
     setTimeout(() => {
         button.classList.remove('active');
-        getText().classList.remove('active');
+        getText('#vord .text').classList.remove('active');
     }, 200);
 }
 
 const onClickVerdade = (button) => {
     const tempV = getRandomElement(verdades);
-    getText().innerHTML = tempV;
+    getText('#vord .text').innerHTML = tempV;
 
     if (tempV == 'verdade agora não é uma opção') {
-        getBtnV().setAttribute("disabled", "");
+        getBtnVVord().setAttribute("disabled", "");
     }
 
-    getText().classList.add('active');
+    getText('#vord .text').classList.add('active');
     button.classList.add('active');
 
     setTimeout(() => {
         button.classList.remove('active');
-        getText().classList.remove('active');
+        getText('#vord .text').classList.remove('active');
+    }, 200);
+}
+
+//------------------------------ EU NUNCA ------------------------------
+
+const onClickEuNunca = (button) => {
+    const tempEN = getRandomElement(euNunca);
+    getText('#never .text').innerHTML = tempEN;
+
+    getText('#never .text').classList.add('active');
+    button.classList.add('active');
+
+    setTimeout(() => {
+        button.classList.remove('active');
+        getText('#never .text').classList.remove('active');
     }, 200);
 }
